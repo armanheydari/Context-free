32662a697fa8702878a19feb54eef7d384730ab2
arman <armanalaki@gmail.com>
2020-05-16 19:20:28 +0430

    start the project

---
+++ Grammer.py
@@ -0,0 +1,22 @@
+class Grammer:
+    def __init__(self):
+        #check is it chomsky
+        self.isChomskyForm = False
+        
+        #check is it Greibach
+        self.isGreibachNormalForm = False
+        
+        #check delete trash
+        self.isDeleteTrash = False
+
+    def ChangeToGreibachForm(self):
+        pass
+
+    def ChangeToChomskyForm(self):
+        pass
+
+    def DeleteTrash(self):
+        pass
+
+    def IsGenerateByGrammer(self):
+        pass
---
+++ Main.py
@@ -0,0 +1,4 @@
+class Main:
+    from Grammer import Grammer
+    #give inputs
+    myGrammer = Grammer()
